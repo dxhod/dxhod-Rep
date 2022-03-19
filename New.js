9340d891dd008e7fae5b80039704913a1c7b86fc
dxhod <98181214+dxhod@users.noreply.github.com>
2022-03-19 17:19:12 +0200

    Create New.js

---
+++ New.js
@@ -0,0 +1,9 @@
+---
+slug: polkadot-js
+lang: en
+title: Polkadot-JS
+---
+
+The Polkadot-JS project is a collection of tools, interfaces, and libraries around Polkadot and Substrate.
+
+> **Note:** While the project is named after "Polkadot", know that these tools, interfaces, and libraries are fully compatible with any Substrate based chain.
